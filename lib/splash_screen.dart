d1433e015df244fbe587b8b87d118fd66ccaafef
%hqara% <%hibba_qaraman@hotmail.com%>
2023-10-25 19:17:31 -0400

    splash screen for starters and added logo via assets/images/smh.png

---
+++ lib/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Smart Money Handling"),
+        backgroundColor: Colors.green[900],
+      ),
+      backgroundColor: Colors.lightGreen[100],
+      body: Center(
+        child: Container(
+          child: Image.asset(
+            'assets/images/smh.png',
+            width: 250,
+            height: 250,
+          ),
+        ),
+      ),
+    );
+  }
+}
